705453024ba83bb8b78f3b184ffa1a14c42e32c7
René Moser <mail@renemoser.net>
2025-10-02 16:03:48 +0200

    reorganize

--- query.sh
+++ query.sh
@@ -1,4 +1,10 @@
 #/bin/bash
+(
+    cd /tmp
+    rm -f dns_ch.txt ch.txt
 
-dig -y hmac-sha512:tsig-zonedata-ch-public-21-01:stZwEGApYumtXkh73qMLPqfbIDozWKZLkqRvcjKSpRnsor6A6MxixRL6C2HeSVBQNfMW4wer+qjS0ZSfiWiJ3Q== @zonedata.switch.ch +noall +answer +noidnout +onesoa AXFR ch. > _ch.txt
-grep -v "RRSIG" _ch.txt | awk '{print $1}' | sed 's/\.$//' | uniq > ch.txt
+    dig -y hmac-sha512:tsig-zonedata-ch-public-21-01:stZwEGApYumtXkh73qMLPqfbIDozWKZLkqRvcjKSpRnsor6A6MxixRL6C2HeSVBQNfMW4wer+qjS0ZSfiWiJ3Q== @zonedata.switch.ch +noall +answer +noidnout +onesoa AXFR ch. > dns_ch.txt
+    grep -v "RRSIG" dns_ch.txt | awk '{print $1}' | sed 's/\.$//' | uniq > ch.txt
+)
+
+./split /tmp/ch.txt
